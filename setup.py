60451ee8fc5e5dc400fd8f0ceaf8185433f6644c
mKhaiTruong <minhkhai0402@gmail.com>
2024-11-16 23:10:07 +0700

    Initial commit - project setup for Medical Chatbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Projects",
+    version = "0.0.0",
+    author = "TRUONG Bill",
+    author_email = "minhkhai0402@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
